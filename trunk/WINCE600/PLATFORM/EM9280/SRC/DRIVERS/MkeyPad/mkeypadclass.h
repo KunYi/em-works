f7806a280c0c03eaf3fc0385c672b8f0e234fdfb
lqk.sch@gmail.com <lqk.sch@gmail.com@9677f95b-f147-b01e-6ec8-0db75aaa1bab>
2012-07-10 06:26:20 +0000

    EM9283:支持3X3,4X3,4X4,5X4,5X5的矩阵键盘驱动程序.

---
+++ trunk/WINCE600/PLATFORM/EM9280/SRC/DRIVERS/MkeyPad/mkeypadclass.h
@@ -0,0 +1,86 @@
+//------------------------------------------------------------------------------
+//
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+//
+//  Use of this source code is subject to the terms of the Microsoft end-user
+//  license agreement (EULA) under which you licensed this SOFTWARE PRODUCT.
+//  If you did not accept the terms of the EULA, you are not authorized to use
+//  this source code. For a copy of the EULA, please see the LICENSE.RTF on
+//  your install media.
+//
+//------------------------------------------------------------------------------
+//
+//  Copyright (C) 2011,Emtronix, Inc. All Rights Reserved.
+//
+//------------------------------------------------------------------------------
+//
+//  File:  mkeypadclass.h
+//
+//  Header file, for mkeypad driver.
+//
+//------------------------------------------------------------------------------
+
+
+#ifndef __MKPDCLASS_H__
+#define __MKPDCLASS_H__
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//------------------------------------------------------------------------------
+// Defines
+#define MAXIN   5
+#define MAXOUT  5
+
+extern DWORD g_uVkeyCode[MAXIN*MAXOUT];
+
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------
+
+
+class MKPDClass
+{
+
+public:
+
+	MKPDClass(UINT32 dwPollingTimeOut, UINT32 dwMkeyPadFromat );
+	~MKPDClass(void);
+
+	void MKeyPadHandle( );
+	static BOOL g_bMkeyPadIsOpen;
+
+private:
+	int m_nMaxScanIn;
+	int m_nMaxScanOut;
+	UINT32 m_dwPollingTimeOut; 
+	HANDLE m_hThread;
+
+	int m_nKeyDelay;
+	int m_nkeyState;
+	BYTE m_ucScanWord;
+
+	__inline BYTE GetDin( );
+	void PutDout( int nIdx, int nVal=0 );
+	void PutDoutAll( int nVal );
+
+	void SetDelay( int nDelay );
+	int IsTimeOut( );
+
+	void KeyIoInit( int m_nMaxScanIn, int m_nMaxScanOut);
+	void KeyIoDeInit( int m_nMaxScanIn, int m_nMaxScanOut );
+
+};
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif   // __MKPDCLASS_H__
+
